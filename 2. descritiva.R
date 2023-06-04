92722fb374ce56c6a3c808640f7557ded83c931d
Galileu Nobre <63824369+galileunobre@users.noreply.github.com>
2023-06-04 00:29:55 -0300

    Add files via upload

---
+++ 2. descritiva.R
@@ -0,0 +1,24 @@
+### buscando base de dados
+dados <- read.csv("pns_19t.csv", sep = ",", header = T)
+head(dados)
+
+### Analise descritiva da base de dados
+
+# tabela com a Média e desvio padrão das variáves explicativas
+variaveis <- tribble(~Variáveis, ~Médias, ~ Desvio_Padrão,
+        "Idade", mean(dados$Idade, na.rm = T), sd(dados$Idade, na.rm = T),
+        "Salário", mean(dados$Salario, na.rm = T), sd(dados$Salario, na.rm = T),
+        "Mulher", mean(dados$mulher, na.rm = T), sd(dados$mulher, na.rm = T),
+        "Casado",  mean(dados$casado, na.rm = T), sd(dados$casado, na.rm = T),
+        "Não branco", mean(dados$nao_branco, na.rm = T), sd(dados$nao_branco, na.rm = T),
+        "Rural", mean(dados$rural, na.rm = T), sd(dados$rural, na.rm = T),
+        "Redes Sociais", mean(dados$redes_sociais, na.rm = T), sd(dados$redes_sociais, na.rm = T),
+        "Ocupado", mean(dados$ocupado, na.rm = T), sd(dados$ocupado, na.rm = T)
+        )
+
+variaveis
+## Analisando a variável dependente
+
+consultas <- tribble(~Média, ~Desvio_Padrão,
+                     mean(dados$consultas, na.rm = T), sd(dados$consultas, na.rm = T))
+consultas
---
+++ 3. analise.R
@@ -0,0 +1,64 @@
+library(AER)
+library(MASS)
+library(COUNT)
+library(outliers)
+
+attach(dados)
+## rodando o modelo de Poisson
+poisson <- glm(data = dados, consultas ~ Idade + 
+                 Salario + 
+                 mulher + 
+                 casado + 
+                 nao_branco + 
+                 rural + 
+                 redes_sociais + 
+                 ocupado)
+summary(poisson)
+
+# Verificando a distribuição dos resíduos
+res_p <- resid(poisson, type = "pearson")
+sum(res_p^2)/poisson$df.residual
+
+
+# Previsões do modelo ajustado
+prpoisson <- predict(poisson, type = "response")
+plot(prpoisson)
+
+## rodando o modelo binomial negativo
+bi_negativo <- glm.nb(data = dados, consultas ~ Idade + 
+                 Salario + 
+                 mulher + 
+                 casado + 
+                 nao_branco + 
+                 rural + 
+                 redes_sociais + 
+                 ocupado)
+summary(bi_negativo)
+
+# Verificando a distribuição dos resíduos
+res_bn <- resid(bi_negativo, type = "pearson")
+sum(res_p^2)/bi_negativo$df.residual
+
+
+# Previsões do modelo ajustado
+prbn <- predict(bi_negativo, type = "response")
+plot(prpoisson)
+
+probabilidades <- data.frame(prpoisson, prbn)
+plot(probabilidades$prpoisson)
+
+summary(probabilidades)
+
+resultados <- tribble(~Modelo, ~Dist._Residuos, ~AIC,
+                     "Poisson", sum(res_p^2)/poisson$df.residual, AIC(poisson),
+                     "Binomial Negativo", sum(res_p^2)/bi_negativo$df.residual, AIC(bi_negativo)
+                     )
+resultados
+min(resutados$Dist._Residuos)
+
+## Arquivo de resultados 
+
+# Salvando tabelas
+write.table(variaveis, file = "Analise_descritiva.csv")
+write.table(consultas, file = "Estatisticas_consultas.csv")
+write.table(resultados, file = "Resultados.csv")

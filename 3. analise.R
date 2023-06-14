f91e4833a2c6be1cad28de6812e549f4e52f8527
Galileu Nobre <63824369+galileunobre@users.noreply.github.com>
2023-06-13 23:34:25 -0300

    Update 3. analise.R

--- 3. analise.R
+++ 3. analise.R
@@ -1,9 +1,5 @@
 library(AER)
 library(MASS)
-library(COUNT)
-
-### buscando base de dados
-dados <- read.csv("pns_19t.csv", sep = ",", header = T)
 
 attach(dados)
 ## rodando o modelo de Poisson
@@ -22,10 +18,6 @@ res_p <- resid(poisson, type = "pearson")
 sum(res_p^2)/poisson$df.residual
 
 
-# Previsões do modelo ajustado
-prpoisson <- predict(poisson, type = "response")
-plot(prpoisson)
-
 ## rodando o modelo binomial negativo
 bi_negativo <- glm.nb(data = dados, consultas ~ Idade + 
                  Salario + 
@@ -42,23 +34,12 @@ res_bn <- resid(bi_negativo, type = "pearson")
 sum(res_p^2)/bi_negativo$df.residual
 
 
-# Previsões do modelo ajustado
-prbn <- predict(bi_negativo, type = "response")
-plot(prpoisson)
-
-probabilidades <- data.frame(prpoisson, prbn)
-plot(probabilidades$prpoisson)
-
-summary(probabilidades)
-
+# Resultados
 resultados <- tribble(~Modelo, ~Dist._Residuos, ~AIC,
                      "Poisson", sum(res_p^2)/poisson$df.residual, AIC(poisson),
                      "Binomial Negativo", sum(res_p^2)/bi_negativo$df.residual, AIC(bi_negativo)
                      )
 resultados
-min(resutados$Dist._Residuos)
 
-## Arquivo de resultados 
-
-# Salvando tabelas
+# Salvando tabela
 write.table(resultados, file = "Resultados.csv")
